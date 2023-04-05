0b73f0c3b734a9bdcb1f880e916ea64813a96a3c
jetek72 <128065849+jetek72@users.noreply.github.com>
2023-04-06 03:07:31 +0900

    Create gasa.py

---
+++ gasa.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/generate_lyrics', methods=['POST'])
+def generate_lyrics():
+    input_text = request.form.get('input_text')
+    headers = {
+        'Authorization': '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',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "model": "gpt-3.5-turbo",
+        "prompt": input_text,
+        "temperature": 0.7,
+        "max_tokens": 2048,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0
+    }
+
+    response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', headers=headers, data=json.dumps(data))
+    response_json = response.json()
+
+    if 'choices' in response_json:
+        generated_lyrics = response_json['choices'][0]['text']
+        return jsonify({'generated_lyrics': generated_lyrics.strip()})
+    else:
+        return jsonify({'error': 'Failed to generate lyrics'}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
